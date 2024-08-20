4aa37ab6257f624fd3beda5fe93dc2090576a2ad
Murage-Ivy <ivymurage2000@gmail.com>
2024-08-20 11:08:36 +0200

    add basic routing

--- client-server/server.js
+++ client-server/server.js
@@ -17,7 +17,23 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
     res.setHeader('Content-Type', 'text/html')
-    fs.readFile('./client-server/views/index.html', (err, data) => {
+
+    let path = './client-server/views/'
+
+    switch(req.url){
+        case '/':
+            path += 'index.html'
+            break;
+        case '/about':
+            path += 'about.html'
+            break;
+        default:
+            path += 'not-found.html'
+            break;
+    }
+
+
+    fs.readFile(path, (err, data) => {
         if(err){
             console.log(err)
             res.end()
