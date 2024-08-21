0ae92660d828e8b96df2f5c9d1a5084efa8bb66a
Murage-Ivy <ivymurage2000@gmail.com>
2024-08-21 15:30:21 +0200

    chore: add redirects and response to get request using express

--- express/app.js
+++ express/app.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('<p>hello</p>')
-})
+// app.get('/', (req, res) => {
+//     res.send('<p>hello</p>')
+// })
 
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', {root: __dirname})
+})
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', {root: __dirname})
+})
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+})
+app.use((req, res) => {
+    res.sendFile('./views/not-found.html', {root: __dirname})
+})
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
